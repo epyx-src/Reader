36f90388a1de9836489b764ad16a53f990e73f9e
Vincent Fournié <vincent.fournie@epyx.ch>
2012-07-10 11:14:38 +0200

    Removed unused method and added missing import
    Signed-off-by: Vincent Fournié <vincent.fournie@epyx.ch>

--- Sources/CGPDFDocument.m
+++ Sources/CGPDFDocument.m
@@ -92,60 +92,6 @@ CGPDFDocumentRef CGPDFDocumentCreateX(CFURLRef theURL, NSString *password)
 	return thePDFDocRef;
 }
 
-CGPDFDocumentRef _CGPDFDocumentCreateX(CFURLRef theURL, NSString *password)
-{
-#ifdef DEBUGX
-	NSLog(@"%s", __FUNCTION__);
-#endif
-    
-	CGPDFDocumentRef thePDFDocRef = NULL;
-    
-	if (theURL != NULL) // Check for non-NULL CFURLRef
-	{
-		thePDFDocRef = CGPDFDocumentCreateWithURL(theURL);
-        
-		if (thePDFDocRef != NULL) // Check for non-NULL CGPDFDocumentRef
-		{
-			if (CGPDFDocumentIsEncrypted(thePDFDocRef) == TRUE) // Encrypted
-			{
-				// Try a blank password first, per Apple's Quartz PDF example
-                
-				if (CGPDFDocumentUnlockWithPassword(thePDFDocRef, "") == FALSE)
-				{
-					// Nope, now let's try the provided password to unlock the PDF
-                    
-					if ((password != nil) && ([password length] > 0)) // Not blank?
-					{
-						char text[128]; // char array buffer for the string conversion
-                        
-						[password getCString:text maxLength:126 encoding:NSUTF8StringEncoding];
-                        
-						if (CGPDFDocumentUnlockWithPassword(thePDFDocRef, text) == FALSE) // Log failure
-						{
-#ifdef DEBUG
-                            NSLog(@"CGPDFDocumentCreateX: Unable to unlock [%@] with [%@]", theURL, password);
-#endif
-						}
-					}
-				}
-                
-				if (CGPDFDocumentIsUnlocked(thePDFDocRef) == FALSE) // Cleanup unlock failure
-				{
-					CGPDFDocumentRelease(thePDFDocRef), thePDFDocRef = NULL;
-				}
-			}
-		}
-	}
-	else // Log an error diagnostic
-	{
-#ifdef DEBUG
-        NSLog(@"CGPDFDocumentCreateX: theURL == NULL");
-#endif
-	}
-    
-	return thePDFDocRef;
-}
-
 //
 //	BOOL CGPDFDocumentNeedsPassword(CFURLRef, NSString *) function
 //
